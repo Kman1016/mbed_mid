c5a38e2f4a4bd06d3ec7d20744c48eb7dad160f8
Kman1016 <72218646+Kman1016@users.noreply.github.com>
2021-04-07 03:18:25 -0700

    1

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ void generate_wave(){
   float i;
   int midT = (240 - (20 * rate))*10;     // middle sleep
   while (1) {
-      // change the voltage on the digital output pin by 0.1 * (sleep time_
+      
     for (i = 0.0f; i < 1.0f; i += 0.01f) {
       aout = i;
       ThisThread::sleep_for(rate * 1ms);
